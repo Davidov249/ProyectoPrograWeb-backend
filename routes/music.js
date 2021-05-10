26b3bcad9247f99dc6c02cca92b499e302ffbfc6
Davidov249 <davidov249@gmail.com>
2021-05-09 22:00:40 -0600

    Documentacion en Swagger.io

--- routes/music.js
+++ routes/music.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router({mergeParams: true});
-/*const redis = require('redis');
-const redisClient = redis.createClient(6379);*/
 const assert = require('assert');
 fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
@@ -9,6 +7,32 @@ const url = process.env.DBSTRING;
 const ObjectID = require('mongodb').ObjectID;
 
 /* GET specific song */
+/**
+
+ * @swagger
+
+ * /one/{id}:
+ *  get:
+ *    summary: Retrieve a specific song from the playlist.
+ *    tags: [Music]
+ *    description: Retrieve the specified song from the user playlist.
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        description: Song id of the specific song.
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: The specified song.
+ *      403:
+ *        description: Unauthorized
+ *      404:
+ *        description: Song not Found
+ *      500:
+ *        description: Internal Server Error
+ */
 router.get('/one/:id', async function(req, res, next) {
 
     const client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -32,6 +56,32 @@ router.get('/one/:id', async function(req, res, next) {
 });
 
 /* GET user playlist */
+/**
+
+ * @swagger
+
+ * /{userid}:
+ *  get:
+ *    summary: Retrieve users playlist.
+ *    tags: [Music]
+ *    description: Retrieve the users playlist.
+ *    parameters:
+ *      - in: path
+ *        name: userid
+ *        required: true
+ *        description: User id.
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: The user playlist.
+ *      403:
+ *        description: Unauthorized
+ *      404:
+ *        description: Playlist not Found
+ *      500:
+ *        description: Internal Server Error
+ */
 router.get('/:userid', async function(req, res, next) {
     const client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
     var userid = req.params.userid;
@@ -58,6 +108,50 @@ router.get('/test/:id', function(req, res, next) {
 })
 
 /* POST song */
+/**
+ 
+ * @swagger
+
+ * /:
+ *  post:
+ *    summary: Add a song to user playlist.
+ *    tags: [Music]
+ *    requestBody:
+ *      description: Create a json that represents a song you want to add to your playlist.
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              userid:
+ *                type: string
+ *                description: The user ID.
+ *                example: 31f8349f-7edd-47d6-a11d-81384559c84f
+ *              name:
+ *                type: string
+ *                description: The song name.
+ *                example: Another one bites the dust
+ *              author:
+ *                type: string
+ *                description: The name of the song author.
+ *                example: Survivor
+ *              genre:
+ *                type: string
+ *                descritpion: The genre of the song.
+ *                example: Rock
+ *              length:
+ *                type: string
+ *                descritpion: The length of the song.
+ *                example: Rock
+ *    responses:
+ *      201:
+ *        description: The song was added to your playlist
+ *      403:
+ *        description: Unauthorized
+ *      500:
+ *        description: Internal Server Error
+ */
 router.post('/', async function(req, res, next) {
 
     const client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -82,6 +176,59 @@ router.post('/', async function(req, res, next) {
 });
 
 /* PUT(update) specific song */
+/**
+ 
+ * @swagger
+
+ * /{id}:
+ *  put:
+ *    summary: Add a song to user playlist.
+ *    tags: [Music]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        description: Song id.
+ *        schema:
+ *          type: string
+ *    requestBody:
+ *      description: Create a json that represents a song you want to add to your playlist.
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              userid:
+ *                type: string
+ *                description: The user ID.
+ *                example: 31f8349f-7edd-47d6-a11d-81384559c84f
+ *              name:
+ *                type: string
+ *                description: The song name.
+ *                example: Another one bites the dust
+ *              author:
+ *                type: string
+ *                description: The name of the song author.
+ *                example: Survivor
+ *              genre:
+ *                type: string
+ *                descritpion: The genre of the song.
+ *                example: Rock
+ *              length:
+ *                type: string
+ *                descritpion: The length of the song.
+ *                example: Rock
+ *    responses:
+ *      202:
+ *        description: The song was added to your playlist
+ *      403:
+ *        description: Unauthorized
+ *      404:
+ *        description: Song not found
+ *      500:
+ *        description: Internal Server Error
+ */
 router.put('/:id', async function(req, res, next) {
     const client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
 
@@ -112,6 +259,32 @@ router.put('/:id', async function(req, res, next) {
 });
 
 /* DELETE specific song */
+/**
+
+ * @swagger
+
+ * /{id}:
+ *  delete:
+ *    summary: Deletes song.
+ *    tags: [Music]
+ *    description: Deletes song from user playlist.
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        description: Song id.
+ *        schema:
+ *          type: string
+ *    responses:
+ *      202:
+ *        description: The song is deleted from playlist.
+ *      403:
+ *        description: Unauthorized
+ *      404:
+ *        description: Playlist not Found
+ *      500:
+ *        description: Internal Server Error
+ */
 router.delete('/:id', async function(req, res, next) {
 
     const client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
